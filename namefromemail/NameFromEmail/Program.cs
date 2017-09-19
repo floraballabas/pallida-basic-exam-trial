5007e96c417847dd46bae110df8ea5ca369db940
Flora <floraballabas@gmail.com>
2017-09-19 10:13:13 +0200

    Uploaded all the files for the pre-exam

---
+++ namefromemail/NameFromEmail/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+
+namespace NameFromEmail
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a function that takes an email address as input in the following format: firstName.lastName@exam.com
+            //and returns a string that represents the user name in the following format: lastName firstName
+            //example: "elek.viz@exam.com" for this input the output should be: "Viz Elek"
+            //accents does not matter 
+            NameFromEmail();
+            Console.ReadLine();
+        }
+        private static void NameFromEmail()
+        {
+            Console.WriteLine("Please enter your email address in the following format: firstName.lastName@exam.com");
+            string email = Console.ReadLine();
+            string user = email.Substring(0, email.IndexOf("@"));
+
+            string firstName = user.Substring(0, user.IndexOf("."));
+            firstName = char.ToUpper(firstName[0]) + firstName.Substring(1);
+
+            string lastName = user.Substring(user.IndexOf(".")+1);
+            lastName = char.ToUpper(lastName[0]) + lastName.Substring(1);
+
+            Console.WriteLine(lastName +" "+ firstName);
+
+        }
+    }
+}
---
+++ oddfilter/OddFilter/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OddFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an OddFilter function that takes a list as a parameter,
+            // and returns a new list with every odd element from the orignal list
+
+            // In case of the example input above, the given PrintList function should print 1 3 5 
+            OddFilter();
+
+            Console.ReadLine();
+        }
+        private static void OddFilter()
+        {
+            List<int> toBeSorted = new List<int> { };
+            List<int> oddElements = new List<int> { };
+            Console.WriteLine("Please tell me how many numbers you will enter for me to sort out the odd numbers");
+            int numbersCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbersCount; i++)
+            {
+                Console.WriteLine("Please enter a number");
+                int numbers = int.Parse(Console.ReadLine());
+                toBeSorted.Add(numbers);
+            }
+
+            for (int i = 0; i < toBeSorted.Count; i++)
+            {
+                if (toBeSorted[i] % 2 != 0)
+                    oddElements.Add(toBeSorted[i]);
+            }
+            Console.WriteLine("These were the odd numbers in your list:");
+            foreach (var odd in oddElements)
+            {
+                Console.Write(odd);
+            }
+        }
+    }
+}
+
